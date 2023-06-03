e65e508b5402bfb0d8b327d2008bdea1ce3a6f74
Henk Simmelink <henk@sisode.nl>
2023-06-03 11:54:09 +0200

    Add class MyAbstractClass

---
+++ DemoClasses/MyAbstractClass.cs
@@ -0,0 +1,33 @@
+﻿namespace DemoClasses;
+
+public abstract class MyAbstractClass
+{
+  public void Do()
+  {
+    MyAbstractMethod();
+  }
+
+  public abstract void MyAbstractMethod();
+
+  internal string CallingFunction(Baz baz)
+  {
+    return ProtectedFunction(baz.Name);
+  }
+
+  protected virtual string ProtectedFunction(string? value)
+  {
+    if (string.IsNullOrWhiteSpace(value))
+    {
+      return string.Empty;
+    }
+
+    switch (value.ToUpper())
+    {
+      case "ABC":
+      case "XYZ":
+        return string.Empty;
+      default:
+        return value;
+    }
+  }
+}
